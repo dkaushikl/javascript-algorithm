bf9c75e763a62e6e5b4f2611e7880197c594746d
Kaushik <dhameliyakaushik13@gmail.com>
2020-03-21 14:50:14 +0530

    update maximum character

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -32,9 +32,55 @@ function maxCharMethodOne(str) {
 
     const finalResult = resultArray.reduce((result, newValue) => {
         return newValue.value > result.value ? newValue : result;
-    }, { key: '', value: 0 });
+    });
 
     return finalResult.key;
 }
 
-module.exports = { maxCharMethodOne };
+function maxCharMethodTwo(str) {
+    if (!utils.checkStringEmpty(str))
+        return '';
+
+    const resultArray = utils.convertToString(str).toLowerCase().split("").reduce((finalResult, newChar) => {
+        const keyIndex = finalResult.findIndex(x => x.key === newChar);
+        if (keyIndex >= 0) {
+            finalResult[keyIndex].value = finalResult[keyIndex].value + 1;
+        } else {
+            finalResult.push({
+                key: newChar,
+                value: 1,
+            });
+        }
+        return finalResult;
+    }, []).reduce((prev, current) => prev.value > current.value ? prev : current)
+
+    return resultArray.key;
+}
+
+function maxCharMethodThree(str) {
+    if (!utils.checkStringEmpty(str))
+        return '';
+
+    const charMap = {};
+    let max = 0;
+    let maxChar = '';
+
+    for (const char of str.toLowerCase()) {
+        if (charMap[char]) {
+            charMap[char]++;
+        } else {
+            charMap[char] = 1;
+        }
+    }
+
+    for (const char in charMap) {
+        if (charMap[char] > max) {
+            max = charMap[char];
+            maxChar = char;
+        }
+    }
+
+    return maxChar;
+}
+
+module.exports = { maxCharMethodOne, maxCharMethodTwo, maxCharMethodThree };
