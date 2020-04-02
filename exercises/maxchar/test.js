30485a4a944d6d2a64fa92dcc46d64db8d556b52
Kaushik <dhameliyakaushik13@gmail.com>
2020-04-02 15:50:34 +0530

    update maximum character

--- exercises/maxchar/test.js
+++ exercises/maxchar/test.js
@@ -1,4 +1,4 @@
-const { maxCharMethodOne } = require('./index.js');
+const { maxCharMethodOne, maxCharMethodTwo, maxCharMethodThree } = require('./index.js');
 
 test('maximum character using method 1', () => {
     expect(maxCharMethodOne("")).toEqual("");
@@ -11,4 +11,25 @@ test('maximum character using method 1', () => {
     expect(maxCharMethodOne("@@@123")).toEqual("@");
 });
 
+test('maximum character using method 2', () => {
+    expect(maxCharMethodTwo("")).toEqual("");
+    expect(maxCharMethodTwo("null")).toEqual("l");
+    expect(maxCharMethodTwo(null)).toEqual("");
+    expect(maxCharMethodTwo(undefined)).toEqual("");
+    expect(maxCharMethodTwo("kaushik")).toEqual("k");
+    expect(maxCharMethodTwo("helllllo")).toEqual("l");
+    expect(maxCharMethodTwo("55555kaushik")).toEqual("5");
+    expect(maxCharMethodTwo("@@@123")).toEqual("@");
+});
+
+test('maximum character using method 3', () => {
+    expect(maxCharMethodThree("")).toEqual("");
+    expect(maxCharMethodThree("null")).toEqual("l");
+    expect(maxCharMethodThree(null)).toEqual("");
+    expect(maxCharMethodThree(undefined)).toEqual("");
+    expect(maxCharMethodThree("kaushik")).toEqual("k");
+    expect(maxCharMethodThree("helllllo")).toEqual("l");
+    expect(maxCharMethodThree("55555kaushik")).toEqual("5");
+    expect(maxCharMethodThree("@@@123")).toEqual("@");
+});
 
