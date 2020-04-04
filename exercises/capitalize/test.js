ce460da513463d8b0a2761cafc2139dce16bb09e
Kaushik <dhameliyakaushik13@gmail.com>
2020-04-04 13:37:34 +0530

    add new test cases in capitalize method

--- exercises/capitalize/test.js
+++ exercises/capitalize/test.js
@@ -5,6 +5,9 @@ test('capitalize using method 1', () => {
     expect(capitalizeOne('it is a great day')).toEqual('It Is A Great Day');
     expect(capitalizeOne('it was warm day')).toEqual('It Was Warm Day');
     expect(capitalizeOne('it is black day')).toEqual('It Is Black Day');
+    expect(capitalizeOne('I’m a little tea pot')).toEqual('I’m A Little Tea Pot');
+    expect(capitalizeOne('sHoRt AnD sToUt')).toEqual('Short And Stout');
+    expect(capitalizeOne('HERE IS MY HANDLE HERE IS MY SPOUT')).toEqual('Here Is My Handle Here Is My Spout');
 });
 
 test('capitalize using method 2', () => {
@@ -12,6 +15,9 @@ test('capitalize using method 2', () => {
     expect(capitalizeTwo('it is a great day')).toEqual('It Is A Great Day');
     expect(capitalizeTwo('it was warm day')).toEqual('It Was Warm Day');
     expect(capitalizeTwo('it is black day')).toEqual('It Is Black Day');
+    expect(capitalizeTwo('I’m a little tea pot')).toEqual('I’m A Little Tea Pot');
+    expect(capitalizeTwo('sHoRt AnD sToUt')).toEqual('Short And Stout');
+    expect(capitalizeTwo('HERE IS MY HANDLE HERE IS MY SPOUT')).toEqual('Here Is My Handle Here Is My Spout');
 });
 
 test('capitalize using method 3', () => {
@@ -19,6 +25,9 @@ test('capitalize using method 3', () => {
     expect(capitalizeThree('it is a great day')).toEqual('It Is A Great Day');
     expect(capitalizeThree('it was warm day')).toEqual('It Was Warm Day');
     expect(capitalizeThree('it is black day')).toEqual('It Is Black Day');
+    expect(capitalizeThree('I’m a little tea pot')).toEqual('I’m A Little Tea Pot');
+    expect(capitalizeThree('sHoRt AnD sToUt')).toEqual('Short And Stout');
+    expect(capitalizeThree('HERE IS MY HANDLE HERE IS MY SPOUT')).toEqual('Here Is My Handle Here Is My Spout');
 });
 
 test('capitalize using method 4', () => {
@@ -26,6 +35,9 @@ test('capitalize using method 4', () => {
     expect(capitalizeForth('it is a great day')).toEqual('It Is A Great Day');
     expect(capitalizeForth('it was warm day')).toEqual('It Was Warm Day');
     expect(capitalizeForth('it is black day')).toEqual('It Is Black Day');
+    expect(capitalizeForth('I’m a little tea pot')).toEqual('I’m A Little Tea Pot');
+    expect(capitalizeForth('sHoRt AnD sToUt')).toEqual('Short And Stout');
+    expect(capitalizeForth('HERE IS MY HANDLE HERE IS MY SPOUT')).toEqual('Here Is My Handle Here Is My Spout');
 });
 
 test('capitalize using method 5', () => {
@@ -33,6 +45,9 @@ test('capitalize using method 5', () => {
     expect(capitalizeFifth('it is a great day')).toEqual('It Is A Great Day');
     expect(capitalizeFifth('it was warm day')).toEqual('It Was Warm Day');
     expect(capitalizeFifth('it is black day')).toEqual('It Is Black Day');
+    expect(capitalizeFifth('I’m a little tea pot')).toEqual('I’m A Little Tea Pot');
+    expect(capitalizeFifth('sHoRt AnD sToUt')).toEqual('Short And Stout');
+    expect(capitalizeFifth('HERE IS MY HANDLE HERE IS MY SPOUT')).toEqual('Here Is My Handle Here Is My Spout');
 });
 
 test('capitalize using method 6', () => {
@@ -40,4 +55,7 @@ test('capitalize using method 6', () => {
     expect(capitalizeSixth('it is a great day')).toEqual('It Is A Great Day');
     expect(capitalizeSixth('it was warm day')).toEqual('It Was Warm Day');
     expect(capitalizeSixth('it is black day')).toEqual('It Is Black Day');
+    expect(capitalizeSixth('I’m a little tea pot')).toEqual('I’m A Little Tea Pot');
+    expect(capitalizeSixth('sHoRt AnD sToUt')).toEqual('Short And Stout');
+    expect(capitalizeSixth('HERE IS MY HANDLE HERE IS MY SPOUT')).toEqual('Here Is My Handle Here Is My Spout');
 });
