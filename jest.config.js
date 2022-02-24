969e0587bd0d06e42d0d7e6b520ea40225b60ee5
dkaushikl <dhameliyakaushik13@gmail.com>
2022-02-25 01:19:34 +0530

    refactor: remove circleci, github action and doc package

--- jest.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testPathIgnorePatterns: ['data-structure', 'ctci', 'dfs', 'js-concept', 'leetcode', 'maths', 'projecteuler'],
-};
